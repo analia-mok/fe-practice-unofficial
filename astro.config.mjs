94cfc175a2938a8a38d830f5e70c6ddda6d71caf
Analia Mok <analiam319@gmail.com>
2022-05-02 20:07:31 -0400

    chore: Initial commit and project scaffolding

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+import turbolinks from "@astrojs/turbolinks";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [turbolinks()]
+});
